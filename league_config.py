af42b570f9f3e70b157dfe400142926bac85fc6a
Daniel Dickinson <d_dickinson@hotmail.com>
2026-08-15 22:17:24 +0100

    Fix watchlist ranking and add league roster config

---
+++ league_config.py
@@ -0,0 +1,17 @@
+BUSY_WORKING_ROSTER = {
+    "QB": 1,
+    "RB": 2,
+    "WR": 2,
+    "TE": 1,
+    "FLEX": 1,
+    "K": 1,
+    "DEF": 1,
+    "BN": 5,
+    "IR": 1,
+}
+
+FLEX_ELIGIBLE = {
+    "RB",
+    "WR",
+    "TE",
+}
