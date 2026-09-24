fd5da332c4084aaca4875295be23c551adb0ada2
Footix <d_dickinson@hotmail.com>
2026-09-24 16:23:19 +0100

    Test speculative waiver risk grading

--- tests/test_weekly_engine_current_week.py
+++ tests/test_weekly_engine_current_week.py
@@ -5,6 +5,7 @@ from unittest.mock import patch
 import weekly_engine
 from weekly_engine import (
     add_transaction_deadlines,
+    build_speculative_waiver_moves,
     build_status_watch,
     cached_start_sit_evidence,
     has_played,
@@ -183,6 +184,143 @@ class WeeklyEngineCurrentWeekTests(TestCase):
                 "transaction_deadline_note"
             ],
             "Packers locks first",
+        )
+
+    def test_small_speculative_waiver_upside_is_not_worth_predrop(self):
+        move = {
+            "week_gain": 0.55,
+            "move_type": "DST STREAM",
+            "add": {
+                "name": "Eagles",
+                "position": "DST",
+                "roster_status": "W (Sep 25)",
+                "yahoo_player_id": "eagles",
+                "current_week_projection": 7.32,
+                "local_game": {
+                    "datetime": datetime(
+                        2099,
+                        9,
+                        27,
+                        18,
+                        0,
+                        tzinfo=weekly_engine.UK_TIME,
+                    )
+                },
+            },
+            "drop": {
+                "name": "Packers",
+                "position": "DST",
+                "yahoo_player_id": "packers",
+                "current_week_projection": 6.77,
+                "local_game": {
+                    "datetime": datetime(
+                        2099,
+                        9,
+                        25,
+                        1,
+                        15,
+                        tzinfo=weekly_engine.UK_TIME,
+                    )
+                },
+            },
+        }
+
+        available = [
+            move["add"],
+            {
+                "name": "Bears",
+                "position": "DST",
+                "roster_status": "FA",
+                "yahoo_player_id": "bears",
+                "current_week_projection": 6.60,
+                "status": None,
+            },
+        ]
+
+        output = build_speculative_waiver_moves(
+            [move],
+            available,
+            2099,
+        )
+
+        self.assertEqual(
+            len(output),
+            1,
+        )
+        self.assertEqual(
+            output[0]["speculative_verdict"],
+            "NOT WORTH PRE-DROP",
+        )
+        self.assertEqual(
+            output[0]["fallback"]["name"],
+            "Bears",
+        )
+        self.assertAlmostEqual(
+            output[0]["fallback_delta"],
+            -0.17,
+            places=2,
+        )
+
+    def test_large_speculative_upside_with_safe_fallback_is_worth_reviewing(self):
+        move = {
+            "week_gain": 8.0,
+            "move_type": "DST STREAM",
+            "add": {
+                "name": "Eagles",
+                "position": "DST",
+                "roster_status": "W (Sep 25)",
+                "yahoo_player_id": "eagles",
+                "current_week_projection": 14.77,
+                "local_game": {
+                    "datetime": datetime(
+                        2099,
+                        9,
+                        27,
+                        18,
+                        0,
+                        tzinfo=weekly_engine.UK_TIME,
+                    )
+                },
+            },
+            "drop": {
+                "name": "Packers",
+                "position": "DST",
+                "yahoo_player_id": "packers",
+                "current_week_projection": 6.77,
+                "local_game": {
+                    "datetime": datetime(
+                        2099,
+                        9,
+                        25,
+                        1,
+                        15,
+                        tzinfo=weekly_engine.UK_TIME,
+                    )
+                },
+            },
+        }
+
+        available = [
+            move["add"],
+            {
+                "name": "Bears",
+                "position": "DST",
+                "roster_status": "FA",
+                "yahoo_player_id": "bears",
+                "current_week_projection": 6.50,
+                "status": None,
+            },
+        ]
+
+        output = build_speculative_waiver_moves(
+            [move],
+            available,
+            2099,
+        )
+
+        self.assertEqual(
+            output[0]["speculative_verdict"],
+            "WORTH REVIEWING",
         )
 
     def test_start_sit_evidence_is_cached_for_same_snapshot(self):
